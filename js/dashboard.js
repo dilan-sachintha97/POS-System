33d22f134dbe7bed730d3782688cddc712f7ae03
dilan-sachintha97 <dil97an@gmail.com>
2022-12-27 14:51:53 +0530

    loaded user details to dashboard

---
+++ js/dashboard.js
@@ -0,0 +1,9 @@
+loadUserDetails=()=>{
+
+    //get user details form browser memory
+    let userDetails = JSON.parse(localStorage.getItem('user'))
+    // console.log( userDetails)
+
+    $('.user-name').html(userDetails.name)   // set username to dashboard
+    $('#avatar').attr('src',userDetails.avatar)  // set avatar to dashboard
+}
