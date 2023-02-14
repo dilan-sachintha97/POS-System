9052648faaab30bc885ebb81937f985312ef9bf4
dilan-sachintha97 <dil97an@gmail.com>
2023-02-14 22:45:27 +0530

    load customer page on dashboard

--- js/customer.js
+++ js/customer.js
@@ -1,11 +1,39 @@
 let customers=[];
+
+//constructor
+function Customer(id,name,address,salary){
+    this.id = id;
+    this.name = name;
+    this.address = address;
+    this.salary = salary
+}
+
 initializeCustomers=()=>{
-    let tempData = JSON.parse(localStorage.getItem('customers'));
+    let tempData = JSON.parse(localStorage.getItem('customer'));
     if(tempData !== null){
-        console.log(tempData);
+        // console.log(tempData);
     }
 }
 
 function saveCustomers(){
+    // console.log("saveCustomer")
+    let customer1 = new Customer(
+        $('#customerId').val(),
+        $('#customerName').val(),
+        $('#customerAddress').val(),
+        Number($('#customerSalary').val())
+    );
+    customers.push(customer1);
 
+    // stored customers array in local store
+    localStorage.setItem('customer',JSON.stringify(customers))
+    clearField();
+
+}
+
+clearField=()=>{
+        $('#customerId').val("");
+        $('#customerName').val("");
+        $('#customerAddress').val("");
+        $('#customerSalary').val("");
 }
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,19 @@
 loadUserDetails=()=>{
     //get user details form browser memory and set to dashboard
     let userDetails = JSON.parse(localStorage.getItem('user'))
-    console.log( userDetails)
+    // console.log( userDetails)
 
     $('.user-name').html(userDetails.name)   // set username to dashboard
     $('#avatar').attr('src',userDetails.avatar)  // set avatar to dashboard
+
+    //dashbord akatath => customer.html page akama set krnna
+    setUi('customer.html');
 }
 
 // to load customer, item, placeOrder, ......
 setUi=(address)=>{
+    if(address === 'customer.html'){
+        initializeCustomers();
+    }
     $('.context').load(address);
 }
